0b4d5df3d53c03f8d92afe852362b45b9a1685e4
Gabriel Rottet <gabegolem@gmail.com>
2025-11-30 18:09:12 -0500

    Created backlog, added sound files, commented data_reading

--- Software/data_reading/data_reading.ino
+++ Software/data_reading/data_reading.ino
@@ -7,6 +7,7 @@
 
 int piezo_pin = A0;
 
+//Struct to store/send data
 typedef struct __attribute__((packed)) struct_readings {
     double acceleration_x;
     double acceleration_y;
@@ -17,25 +18,21 @@ typedef struct __attribute__((packed)) struct_readings {
     double piezo;
 } struct_readings;
 
-// Create a variable of this type to store the data
 struct_readings readings;
 char * readings_bytes;
-char * gyro_x_bytes;
-char * gyro_y_bytes;
 
-// Laptop's MAC Address (replace with the actual MAC address)
+//Network Info
 const char* ssid = "Home524";
 const char* pword = "Home7777";
-uint8_t broadcastAddress[] = {0xdc, 0x21, 0x48, 0x82, 0x96, 0x82}; //mac address
-const char* host = "10.0.0.210";//implement automatic grabbing of laptop ip
+const char* host = "10.0.0.210"; 
 const uint16_t port = 8000;
-bool itison = false;
+uint8_t broadcastAddress[] = {0xdc, 0x21, 0x48, 0x82, 0x96, 0x82}; //mac address - currently unused
 
-/* Assign a unique ID to this sensor at the same time */
+// Initialize both accelerometer and gyroscope components of the IMU
 Adafruit_FXOS8700 accelmag = Adafruit_FXOS8700(0x8700A, 0x8700B);
-
 Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);
 
+// prints stats of sensor components
 void displaySensorDetails(void) {
 
   sensor_t gyro_sensor;
@@ -81,16 +78,21 @@ void displaySensorDetails(void) {
   delay(500);
 }
 
+
 void setup(void) {
   Serial.begin(115200);
 
+  // Connects ESP to network
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pword);
 
+  // Waits while connecting
   while(WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print("_");
   }
+  
+  //Displays network ssid upon connecting
   Serial.println("");
   Serial.println(WiFi.SSID());
 
@@ -99,45 +101,43 @@ void setup(void) {
     delay(1);
   }
 
+  // Sets up IMU
   Serial.println("Gyroscope Test");
   Serial.println("");
 
   /* Initialise the sensor */
   if (!gyro.begin()) {
-    /* There was a problem detecting the FXAS21002C ... check your connections
-     */
     Serial.println("Ooops, no FXAS21002C detected ... Check your wiring!");
     while (1)
       ;
   }
 
+  // Sets gyro readings to max range of 2000 degrees per second
   gyro.setRange(GYRO_RANGE_2000DPS);
+
 
   Serial.println("Accelerometer Test");
   Serial.println("");
 
   /* Initialise the sensor */
   if (!accelmag.begin()) {
-    /* There was a problem detecting the FXAS21002C ... check your connections
-     */
     Serial.println("Ooops, no FXOS8700 detected ... Check your wiring!");
     while (1)
       ;
   }
-
+  
+  // Sets accel readings to max range of 8g
   accelmag.setAccelRange(ACCEL_RANGE_8G);
   accelmag.setSensorMode(ACCEL_ONLY_MODE);
-
+  
+  // Displays sensor details, indicating successful setup
   displaySensorDetails();
 }
 
+
 void loop(void) {
 
-  if(itison) {
-    delay(2000);
-    return;
-  }
-
+  //Connects to specified address/port
   WiFiClient client;
   if (!client.connect(host, port)) {
     Serial.println("Connection Failed");
@@ -145,13 +145,13 @@ void loop(void) {
     return;
   }
 
+  // creates and reads data from imu
   sensors_event_t accelmag_event;
   accelmag.getEvent(&accelmag_event);
   sensors_event_t gyro_event;
   gyro.getEvent(&gyro_event);
 
-  /* Display the results (speed is measured in rad/s) */
-
+  // writes imu data to struct
   readings.gyro_x = gyro_event.gyro.x;
   readings.gyro_y = gyro_event.gyro.y;
   readings.gyro_z = gyro_event.gyro.z;
@@ -159,18 +159,22 @@ void loop(void) {
   readings.acceleration_y = accelmag_event.acceleration.y;
   readings.acceleration_z = accelmag_event.acceleration.z;
   readings.piezo = analogRead(piezo_pin);
-  readings_bytes = reinterpret_cast<char*>(&readings);
+  readings_bytes = reinterpret_cast<char*>(&readings); //converts struct data types to raw bytes
 
+  // Attempts to send bytes to server
   while(client.available() > 0 || client.connected()>0) {
     //client.write(readings_bytes, sizeof(double) * 7);
     client.write(readings_bytes, 56);
   }
+
+  // Delay sets data reading rate. Too fast overwhelms server and results in crashing
   delay(2.5);
 
-  displayData(gyro_event, accelmag_event);
-  /* Get a new sensor event */
+  
+  //displayData(gyro_event, accelmag_event);
 }
 
+// Displays data reading to serial for debugging purposes
 void displayData(sensors_event_t gyro_event, sensors_event_t accelmag_event) {
   Serial.print("GYRO  ");
   Serial.print("X: ");
--- Software/data_reading/server.py
+++ Software/data_reading/server.py
@@ -4,25 +4,29 @@ import csv
 from collections import deque
 import time
 
-NUM_READINGS = 40
-NUM_READINGS_AFTER = 10
-PIEZO_THRESHOLD = 1000
+NUM_READINGS = 40              #Readings per punch
+NUM_READINGS_AFTER = 10        #Number of readings following impact 
+PIEZO_THRESHOLD = 1000         #Threshold to register/record a punch 
 CSV = "../modeling/sample.csv" #"../modeling/profiles/test_profile/data/right_hook.csv"
 
 
-format_string = '<7d'
-struct_size = struct.calcsize(format_string)
+format_string = '<7d'          #Sets byte reading to 7 little-endian floats 
+struct_size = struct.calcsize(format_string) #Sets size to size of 7 floats (56 bytes) 
 
-queue = deque(maxlen=NUM_READINGS)
+queue = deque(maxlen=NUM_READINGS) #Creates queue to store readings
 
+'''Sets up csv'''
 csv_file = open(CSV,'a',newline='')
 csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+
+'''Sets up tcp socket communication'''
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = ("10.0.0.210", 8000)
 server_socket.bind(address)
 print("Listening...")
 server_socket.listen(1)
 
+'''Displays received data for debugging'''
 def displayData(unpacked_data): 
         acceleration_x = unpacked_data[0]
         acceleration_y = unpacked_data[1]
@@ -41,15 +45,16 @@ def displayData(unpacked_data):
               \nGyro_z: {gyro_zi}
         """)
 
+
 while True:
     client_socket, client_address = server_socket.accept()
     #print("Accepted")
     try:
-        data_bytes = client_socket.recv(56)
-        unpacked_data = struct.unpack(format_string, data_bytes)
-        queue.append(unpacked_data)
-        if (queue[-NUM_READINGS_AFTER][6] > PIEZO_THRESHOLD):
-            for i in range(len(queue)):
+        data_bytes = client_socket.recv(56)                      #Receives struct_size bytes
+        unpacked_data = struct.unpack(format_string, data_bytes) #Unpacks into list of floats
+        queue.append(unpacked_data) #Adds data to queue, automatically dequeueing extra values
+        if (queue[-NUM_READINGS_AFTER][6] > PIEZO_THRESHOLD):    #If impact threshold is met   
+            for i in range(len(queue)):                          #Writes current queue to csv
                 csv_writer.writerow(queue[i])
 
         #displayData(unpacked_data)
