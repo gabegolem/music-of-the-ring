208aa1a35a381dda1d40067881995c9f3bf16360
Gabriel Rottet <gabegolem@gmail.com>
2025-12-03 23:43:24 -0500

    created udp version of data_reading files

---
+++ Software/data_reading/udp_reading/udp_reading.ino
@@ -0,0 +1,196 @@
+#include <Adafruit_FXAS21002C.h>
+#include <Adafruit_FXOS8700.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+
+int piezo_pin = A0;
+
+//Struct to store/send data
+typedef struct __attribute__((packed)) struct_readings {
+    double acceleration_x;
+    double acceleration_y;
+    double acceleration_z;
+    double gyro_x;
+    double gyro_y;
+    double gyro_z;
+    double piezo;
+} struct_readings;
+
+struct_readings readings;
+uint8_t * readings_bytes;
+
+WiFiUDP Udp;
+
+//Network Info
+const char* ssid = "David’s iPhone (6)";
+//const char* ssid = "bucknell_iot";
+const char* pword = "mybdrppas4atz";
+const char* host = "172.20.10.3"; 
+const uint16_t port = 8000;
+uint8_t broadcastAddress[] = {0xdc, 0x21, 0x48, 0x82, 0x96, 0x82}; //mac address - currently unused
+
+// Initialize both accelerometer and gyroscope components of the IMU
+Adafruit_FXOS8700 accelmag = Adafruit_FXOS8700(0x8700A, 0x8700B);
+Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);
+
+// prints stats of sensor components
+void displaySensorDetails(void) {
+
+  sensor_t gyro_sensor;
+  sensor_t accelmag_sensor;
+
+  gyro.getSensor(&gyro_sensor);
+  accelmag.getSensor(&accelmag_sensor);
+  Serial.println("------------------------------------");
+  Serial.print("Gyro-Sensor:       ");
+  Serial.println(gyro_sensor.name);
+  Serial.print("Driver Ver:   ");
+  Serial.println(gyro_sensor.version);
+  Serial.print("Unique ID:    0x");
+  Serial.println(gyro_sensor.sensor_id, HEX);
+  Serial.print("Max Value:    ");
+  Serial.print(gyro_sensor.max_value);
+  Serial.println(" rad/s");
+  Serial.print("Min Value:    ");
+  Serial.print(gyro_sensor.min_value);
+  Serial.println(" rad/s");
+  Serial.print("Resolution:   ");
+  Serial.print(gyro_sensor.resolution);
+  Serial.println(" rad/s");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  Serial.println("------------------------------------");
+  Serial.print("Accelmag-Sensor:       ");
+  Serial.println(accelmag_sensor.name);
+  Serial.print("Driver Ver:   ");
+  Serial.println(accelmag_sensor.version);
+  Serial.print("Unique ID:    0x");
+  Serial.println(accelmag_sensor.sensor_id, HEX);
+  Serial.print("Max Value:    ");
+  Serial.print(accelmag_sensor.max_value);
+  Serial.println(" m/s^2");
+  Serial.print("Min Value:    ");
+  Serial.print(accelmag_sensor.min_value);
+  Serial.println(" m/s^2");
+  Serial.print("Resolution:   ");
+  Serial.print(accelmag_sensor.resolution);
+  Serial.println(" m/s^2");
+  Serial.println("------------------------------------");
+  delay(500);
+}
+
+
+void setup(void) {
+  Serial.begin(115200);
+
+  // Connects ESP to network
+  WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid); 
+  Serial.print("\nWifi_status: "); Serial.print(WiFi.begin(ssid, pword));
+  Serial.print("\nUDP_status: "); Serial.print(Udp.begin(4000));
+  // Waits while connecting
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("_");
+  }
+
+  //Displays network ssid upon connecting
+  Serial.println("");
+  Serial.println(WiFi.SSID());
+
+  /* Wait for the Serial Monitor */
+  while (!Serial) {
+    delay(1);
+  }
+
+  // Sets up IMU
+  Serial.println("Gyroscope Test");
+  Serial.println("");
+
+  /* Initialise the sensor */
+  if (!gyro.begin()) {
+    Serial.println("Ooops, no FXAS21002C detected ... Check your wiring!");
+    while (1)
+      ;
+  }
+
+  // Sets gyro readings to max range of 2000 degrees per second
+  gyro.setRange(GYRO_RANGE_2000DPS);
+
+
+  Serial.println("Accelerometer Test");
+  Serial.println("");
+
+  /* Initialise the sensor */
+  if (!accelmag.begin()) {
+    Serial.println("Ooops, no FXOS8700 detected ... Check your wiring!");
+    while (1)
+      ;
+  }
+  
+  // Sets accel readings to max range of 8g
+  accelmag.setAccelRange(ACCEL_RANGE_8G);
+  accelmag.setSensorMode(ACCEL_ONLY_MODE);
+  
+  // Displays sensor details, indicating successful setup
+  displaySensorDetails();
+}
+
+
+void loop(void) {
+
+  // creates and reads data from imu
+  sensors_event_t accelmag_event;
+  accelmag.getEvent(&accelmag_event);
+  sensors_event_t gyro_event;
+  gyro.getEvent(&gyro_event);
+
+  // writes imu data to struct
+  readings.gyro_x = gyro_event.gyro.x;
+  readings.gyro_y = gyro_event.gyro.y;
+  readings.gyro_z = gyro_event.gyro.z;
+  readings.acceleration_x = accelmag_event.acceleration.x;
+  readings.acceleration_y = accelmag_event.acceleration.y;
+  readings.acceleration_z = accelmag_event.acceleration.z;
+  readings.piezo = analogRead(piezo_pin);
+  readings_bytes = reinterpret_cast<uint8_t*>(&readings); //converts struct data types to raw bytes
+
+  // Attempts to send bytes to server
+  Udp.beginPacket(host, port);
+  Udp.write(readings_bytes, 56);
+  Udp.endPacket();
+
+  // Delay sets data reading rate. Too fast overwhelms server and results in crashing
+  delay(1);
+  //displayData(gyro_event, accelmag_event);
+}
+
+// Displays data reading to serial for debugging purposes
+void displayData(sensors_event_t gyro_event, sensors_event_t accelmag_event) {
+  Serial.print("GYRO  ");
+  Serial.print("X: ");
+  Serial.print(gyro_event.gyro.x);
+  Serial.print("  ");
+  Serial.print("Y: ");
+  Serial.print(gyro_event.gyro.y);
+  Serial.print("  ");
+  Serial.print("Z: ");
+  Serial.print(gyro_event.gyro.z);
+  Serial.print("  ");
+  Serial.println("rad/s ");
+  Serial.print("ACCELERATION  ");
+  Serial.print("X: ");
+  Serial.print(accelmag_event.acceleration.x);
+  Serial.print("  ");
+  Serial.print("Y: ");
+  Serial.print(accelmag_event.acceleration.y);
+  Serial.print("  ");
+  Serial.print("Z: ");
+  Serial.print(accelmag_event.acceleration.z);
+  Serial.print("  ");
+  Serial.println("m/s^2");
+}
---
+++ Software/data_reading/udp_receiver.py
@@ -0,0 +1,67 @@
+import socket
+import struct
+import csv
+from collections import deque
+import time
+
+NUM_READINGS = 100              #Readings per punch
+NUM_READINGS_AFTER = 5        #Number of readings following impact 
+PIEZO_THRESHOLD = 1000         #Threshold to register/record a punch 
+CSV = "../modeling/sample.csv" #"../modeling/profiles/test_profile/data/right_hook.csv"
+
+
+format_string = '<7d'          #Sets byte reading to 7 little-endian floats 
+struct_size = struct.calcsize(format_string) #Sets size to size of 7 floats (56 bytes) 
+
+queue = deque(maxlen=NUM_READINGS) #Creates queue to store readings
+
+'''Sets up csv'''
+csv_file = open(CSV,'a',newline='')
+csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+
+'''Sets up udp socket communication'''
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = ''
+port = 8000
+address = ('', port)
+sock.bind(address)
+cooldown = 0
+print("Waiting...")
+prev = time.time()
+'''Displays received data for debugging'''
+def displayData(unpacked_data): 
+        acceleration_x = unpacked_data[0]
+        acceleration_y = unpacked_data[1]
+        acceleration_z = unpacked_data[2]
+        gyro_x = unpacked_data[3]
+        gyro_y = unpacked_data[4]
+        gyro_z = unpacked_data[5]
+        piezo = unpacked_data[6]
+        csv_writer.writerow(unpacked_data)
+        print(f"""Received from client: 
+              \nAcc_x: {acceleration_x}
+              \nAcc_y: {acceleration_y}
+              \nAcc_z: {acceleration_z}
+              \nGyro_x: {gyro_x}
+              \nGyro_y: {gyro_y}
+              \nGyro_z: {gyro_z}
+        """)
+
+
+while True:
+    current = time.time()
+    data_bytes, address = sock.recvfrom(56)                      #Receives struct_size bytes
+    print(f"Accepted: {(current-prev) * 1000}")
+    unpacked_data = struct.unpack(format_string, data_bytes) #Unpacks into list of floats
+    queue.append(unpacked_data) #Adds data to queue, automatically dequeueing extra values
+    if (queue[0][6] > PIEZO_THRESHOLD and (cooldown == 0)):#If impact threshold is met   
+        cooldown = 100
+        for i in range(len(queue)):                          #Writes current queue to csv
+            csv_writer.writerow(queue[i])
+    else:
+        if (cooldown > 0):
+            cooldown -= 1
+    prev = current
+    #displayData(unpacked_data)
+csv_file.close()
+sock.close()
